c8a5e824ba9afe37a08b463f7b6be6a3ec895e27
ChristianDuda <duda.kristian.ns@gmail.com>
2022-11-29 16:35:39 +0200

    Added submit handler function

--- src/components/btnSubmit/btnSubmit.component.tsx
+++ src/components/btnSubmit/btnSubmit.component.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export const BtnSubmit: React.FC = () => {
+
+    const onSubmitHandler = (event:React.FormEvent<HTMLButtonElement>):void  => {
+        event.preventDefault()
+    }
+
+
     return (
         <div>
-            <button type='submit'>Add new Contact</button>
+            <button
+                type='submit'
+                onSubmit={onSubmitHandler}
+            >
+                Add new Contact
+            </button>
         </div>
     )
 }
