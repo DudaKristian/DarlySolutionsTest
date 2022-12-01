3006194517270f4998fdb7034437d2ce2403eab8
ChristianDuda <duda.kristian.ns@gmail.com>
2022-12-01 19:59:00 +0200

    Created modal toggle button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContactAddForm } from '../../components/contactAddForm/contactAddForm';
 import { Table } from '../../components/table/table.component';
 
@@ -7,17 +7,33 @@ import girl from '../../images/bggirl.png'
 import boy from '../../images/bgboy.png'
 
 
-
 export const Home: React.FC = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const modalToggle = () => {
+        setIsModalOpen(!isModalOpen)
+    }
+
+    
 
     return (
         <div className={styles.home__background}>
             <img src={girl} alt="girl" className={styles.home__girl} />
             <img src={boy} alt="boy" className={styles.home__boy} />
-            <div className={styles.home__wrapper}>            
+            <div className={styles.home__wrapper}>
                 <Table />
-                {/* <ContactAddForm /> */}
+                {isModalOpen &&
+                    <ContactAddForm />
+                }
+                <button
+                className={styles.home__addBtn}
+                type='button'
+                onClick={modalToggle}
+                >Add contact
+                </button>
             </div>
+            
         </div>
     )
 }
